ffb8b6accc66240d98413e7b78deacc681e265ef
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2020-10-20 01:02:47 +0530

    imported cascades and tested app

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import cv2
+import numpy as np
+import os
 
-st.title('Face Detection App')
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
+
+
+def main():
+	st.title("Face Detection App")
+	st.text("Built with Streamlit and OpenCV")
+
+
+if __name__ == '__main__':
+	main()
